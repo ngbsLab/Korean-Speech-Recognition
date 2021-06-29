18996df16c6980857b43b60c9c236b08e038e155
ngbsLab <62923168+ngbsLab@users.noreply.github.com>
2021-06-30 03:25:47 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,36 +1,6 @@
 """
 -*- coding: utf-8 -*-
 
-Copyright 2020- Kai.Lib
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-- Korean Speech Recognition
-Team: Kai.Lib
-    ● Team Member
-        ○ Kim-Soo-Hwan: KW University elcomm. senior
-        ○ Bae-Se-Young: KW University elcomm. senior
-        ○ Won-Cheol-Hwang: KW University elcomm. senior
-
-Model Architecture:
-    ● Listen, Attend and Spell (Seq2seq with Attention)
-
-Data:
-    ● A.I Hub Dataset
-
-Score:
-    ● CRR: Character Recognition Rate
-    ● CER: Character Error Rate based on Edit Distance
-
-GitHub repository : https://github.com/sooftware/Korean-Speech-Recognition
-Documentation : https://sooftware.github.io/Korean-Speech-Recognition/index.html
-"""
 
 import queue
 import torch.nn as nn
